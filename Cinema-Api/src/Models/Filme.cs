e5d7271218ab26315cbb643b6e44a0265be7c6a9
Thiago Amaral Costa <thgamrlcsta@gmail.com>
2025-05-19 17:12:41 -0300

    Modelos Feitos.

--- Cinema-Api/src/Models/Filme.cs
+++ Cinema-Api/src/Models/Filme.cs
@@ -1,10 +1,16 @@
 namespace Cinema_Api.src.Models;
 
-public class Filme(string titulo)
+public class Filme(string titulo, int anoLancamento, string sinopse, string posterURLs, float nota)
 {
-	
-	public string Titulo { get;set; } = titulo;
 
-	// Etc...
+	public string Titulo { get; set; } = titulo;
+
+	public int AnoLancamento { get; set; } = anoLancamento;
+
+	public string Sinopse { get; set; } = sinopse;
+
+	public string PosterURLs { get; set; } = posterURLs;
+
+	public float NotaIMDB { get; set; } = nota;
 	
 }
