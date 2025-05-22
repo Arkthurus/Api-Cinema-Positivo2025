e510a5cf326570b97a8c2324f7a33439a45c461b
ARTHUR JOAO PADILHA CONCI <arthur.joao@cs.up.edu.br>
2025-05-22 09:42:41 -0300

    todos objs addcionados, todos Contexts Addcionados

---
+++ Cinema-Api/src/Models/DTOs/FilmeAtor.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema_Api.src.Models;
+
+[PrimaryKey("FilmeId", "AtorId")]
+public class FilmeAtor
+{
+	public int FilmeId { get; set; }
+
+	[JsonIgnore]
+
+	public Filme Filme { get; set; } = null!;
+
+	public int GeneroId { get; set; }
+
+	[JsonIgnoreAttribute]
+	
+	public Genero Genero { get; set; } = null!;
+}
